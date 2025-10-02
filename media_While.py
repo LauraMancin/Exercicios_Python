2f0b9925efc043a3e12e16d3efe7ed9f0828cf06
Laura Mancin <lauramancin8@gmail.com>
2025-10-02 10:23:45 -0300

    Add files via upload

---
+++ media_While.py
@@ -0,0 +1,12 @@
+nota1 = int(input('Insira sua primeira nota: '))
+nota2 = int(input('Insira sua segunda nota: '))
+nota3 = int(input('Insira sua terceira nota: '))
+nota4 = int(input('Insira sua quarta nota: '))
+
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print(f'Sua média é {media} ')
+
+contador = 1
+
+while contador <=5:
+    print('Você está reprovado!')
---
+++ media_While2.py
@@ -0,0 +1,5 @@
+#setar os valores iniciais
+contador = 1
+soma = 0
+
+while contador <=4:
